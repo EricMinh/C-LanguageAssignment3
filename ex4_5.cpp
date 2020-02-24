5644d65df108fd4faf97fc9bad9d82b747592a22
EricMinh <32127944+EricMinh@users.noreply.github.com>
2020-02-24 15:51:27 -0800

    Create ex4_5.cpp

---
+++ ex4_5.cpp
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>		/* for atof() */
+#include <ctype.h>
+#include <math.h>
+#include <string>
+
+#define MAXOP 100		/* max size of operand or operator */
+#define NUMBER '0'		/* signal that a number was found */
+#define MAXVAL 100		/* maximum depth of val stack */
+#define BUFSIZE 100
+#define NAME 'n'
+
+char buf[BUFSIZE];		/* buffer for ungetch */
+int bufp = 0;			/* next free position in buf */
+int sp = 0;				/* next free stack position */
+double val[MAXVAL];		/* value stack */
+
+/* push: push f onto value stack */
+void push(double f)
+{
+	if (sp < MAXVAL)
+		val[sp++] = f;
+	else
+		printf("error: stack full, can't push %g\n", f);
+}
+
+/* pop: pop and return top value from stack */
+double pop(void)
+{
+	if (sp > 0)
+		return val[--sp];
+	else {
+		printf("error: stack empty\n");
+		return 0.0;
+	}
+}
+
+int getch(void) /* get a (possibly pushed-back) character */
+{
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) /* push character back on input */
+{
+	if (bufp >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+}
+
+/* getop: get next character or numeric operand */
+int getop(char s[])
+{
+	int i, c;
+	while ((s[0] = c = getch()) == ' ' || c == '\t')
+		;
+	s[1] = '\0';
+	i = 0;
+
+	if (islower(c)) {
+		while (islower(s[++i] = c = getch()))
+			;
+		s[i] = '\0';
+		if (c != EOF)
+			ungetch(c);
+		if (strlen(s) > 1)
+			return NAME;
+		else
+			return c;
+
+	}
+	if (!isdigit(c) && c != '.')
+		return c; /* not a number */
+	if (isdigit(c)) /* collect integer part */
+		while (isdigit(s[++i] = c = getch()))
+			;
+	if (c == '.') /* collect fraction part */
+		while (isdigit(s[++i] = c = getch()))
+			;
+	s[i] = '\0';
+	if (c != EOF)
+		ungetch(c);
+	return NUMBER;
+}
+
+void mathfnc(char s[]) {
+
+	double op2;
+
+	if (strcmp(s, "sin") == 0)
+		push(sin(pop()));
+	else if (strcmp(s, "cos") == 0)
+		push(cos(pop()));
+	else if (strcmp(s, "exp") == 0)
+		push(exp(pop()));
+	else if (strcmp(s, "pow") == 0) {
+		op2 = pop();
+		push(pow(pop(), op2));
+	}
+	else { printf("error: %s is not supported\n", s); }
+}
+
+/* reverse Polish calculator */
+int main(int argc, const char * argv[])
+{
+	int type;
+	double op2;
+	char s[MAXOP];
+	while ((type = getop(s)) != EOF) {
+		switch (type) {
+		case NUMBER:
+			push(atof(s));
+			break;
+		case NAME:
+			mathfnc(s);
+			break;
+		case '+':
+			push(pop() + pop());
+			break;
+		case '*':
+			push(pop() * pop());
+			break;
+		case '-':
+			op2 = pop();
+			push(pop() - op2);
+			break;
+		case '/':
+			op2 = pop();
+			if (op2 != 0.0)
+				push(pop() / op2);
+			else
+				printf("error: zero divisor\n");
+			break;
+		case '\n':
+			printf("\t%.8g\n", pop());
+			break;
+		default:
+			printf("error: unknown command %s\n", s);
+			break;
+		}
+	}
+	return 0;
+}
