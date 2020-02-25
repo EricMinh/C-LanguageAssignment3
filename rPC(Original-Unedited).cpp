8752bbd9bf2e72c97d6515b29267cb3f987c3c55
EricMinh <32127944+EricMinh@users.noreply.github.com>
2020-02-24 16:44:33 -0800

    Update rPC(Original-Unedited).cpp

--- rPC(Original-Unedited).cpp
+++ rPC(Original-Unedited).cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>		/* for atof() */
 #include <ctype.h>
+#include <math.h>
 
 #define MAXOP 100		/* max size of operand or operator */
 #define NUMBER '0'		/* signal that a number was found */
